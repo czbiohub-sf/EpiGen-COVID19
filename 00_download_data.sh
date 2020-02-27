51088a018d9d9b17c7cdc3f4d12fe9ebeabc2ff6
Lucy Li <lucy.li@li-mbp.local>
2020-02-26 21:14:38 -0800

    clean up data folder

--- 00_download_data.sh
+++ 00_download_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mkdir -p data/CSSE
-wget https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv -O data/CSSE/time_series_19-covid-Confirmed.csv
-wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv -O data/CSSE/time_series_19-covid-Deaths.csv
-wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv -O data/CSSE/time_series_19-covid-Recovered.csv
+mkdir -p data/time_series/CSSE
+wget https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv -O data/time_series/CSSE/time_series_19-covid-Confirmed.csv
+wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv -O data/time_series/CSSE/time_series_19-covid-Deaths.csv
+wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv -O data/time_series/CSSE/time_series_19-covid-Recovered.csv
+
