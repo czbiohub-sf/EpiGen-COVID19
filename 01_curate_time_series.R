0a6c6a3c7db2f225d96ebe44737ca73185ce9e42
Lucy Li <lucy.li@li-mbp.attlocal.net>
2020-03-31 20:20:51 -0700

    select countries and provinces to model

--- 01_curate_time_series.R
+++ 01_curate_time_series.R
@@ -7,14 +7,49 @@ library(lubridate)
 data_dir <- "data/time_series"
 
 
+# Select places to model --------------------------------------------------
+
+
+seq_metadata <- read_tsv("data/sequences/gisaid_metadata.tsv")
+seq_freq_by_province <- seq_by_country <- 
+  with(seq_metadata, coalesce(division_exposure, division, country)) %>%
+  table() %>%
+  sort()
+names_to_change <- names(seq_freq_by_province) %in% seq_metadata$division
+names_to_change_rows <- match(names(seq_freq_by_province)[names_to_change], seq_metadata$division)
+names(seq_by_country)[names_to_change] <- seq_metadata$country[names_to_change_rows]
+seq_by_country <- split(seq_by_country, names(seq_by_country)) %>% sapply(sum) %>% sort()
+
+# Pick the countries with the most sequence data relative to the country's population size
+# and geographic area
+# seq_by_country[seq_by_country>50]
+# seq_freq_by_province[seq_freq_by_province>30]
+
+countries_to_model <- c("Switzerland", "Austria", "Italy", "Japan", "Belgium", 
+                        "France", "Iceland", "Netherlands", "United Kingdom")
+provinces_to_model <- c("Hubei", "Guangdong", "Shanghai", "Ontario", "Washington",
+                        "New York", "Hong Kong", "Minnesota")
+
+
+
 # Import data -------------------------------------------------------------
 
-
 # Primarily use time series curated by Johns Hopkins CSSE team
-csse_confirmed <- read_csv(file.path(data_dir, "CSSE", "time_series_19-covid-Confirmed.csv")) %>%
+csse_confirmed_original <- read_csv(file.path(data_dir, "CSSE", "time_series_covid19_confirmed_global.csv")) %>%
   pivot_longer(., cols=-1:-4, names_to="date", values_to="cumulative_cases") %>%
   mutate(date=as.Date(date, "%m/%d/%y")) %>%
-  rename(province=`Province/State`, country=`Country/Region`, lat=Lat, long=Long)
+  rename(province=`Province/State`, country=`Country/Region`, lat=Lat, long=Long) %>%
+  filter(country!="US")
+
+csse_confirmed_us <- read_csv(file.path(data_dir, "CSSE", "time_series_covid19_confirmed_US.csv")) %>%
+  pivot_longer(., cols=-1:-which(names(.)=="Combined_Key"),
+               names_to="date", values_to="cumulative_cases") %>%
+  mutate(date=as.Date(date, "%m/%d/%y")) %>%
+  rename(province=`Province_State`, country=`Country_Region`, lat=Lat, long=Long_) %>%
+  `[`(., , names(csse_confirmed_original))
+
+csse_confirmed <- csse_confirmed_original %>% bind_rows(csse_confirmed_original, csse_confirmed_us)
+
 
 # For the time period before January 22, use epidemiological data 
 # reported in Li et al (2020) NEJM paper for Hubei
@@ -67,6 +102,7 @@ write_tsv(global_cumulative_cases, file.path(data_dir, "summary_global_timeserie
 
 
 country_specific_cumulative_cases <- cumulative_cases %>%
+  filter(country %in% countries_to_model) %>%
   split(., .$country) %>%
   lapply(function (x) {
     country_name <- x$country[1] %>% tolower() %>% gsub(" ", "", ., fixed = TRUE)
@@ -75,11 +111,18 @@ country_specific_cumulative_cases <- cumulative_cases %>%
     write_tsv(output_df, file.path(data_dir, paste0("summary_", country_name, "_timeseries_cumulative_cases.tsv")))
     output_df
   })
+
+province_specific_cumulative_cases <- cumulative_cases %>%
+  filter(province %in% provinces_to_model) %>%
+  split(., .$province) %>%
+  lapply(function (x) {
+    province_name <- x$province[1] %>% tolower() %>% gsub(" ", "", ., fixed = TRUE)
+    output_df <- group_by(x, date) %>%
+      summarize(cumulative_cases=sum(cumulative_cases))
+    write_tsv(output_df, file.path(data_dir, paste0("summary_", province_name, "_timeseries_cumulative_cases.tsv")))
+    output_df
+  })
   
-
-
-
-
 
 # Create daily incidence time series --------------------------------------
 
@@ -107,6 +150,7 @@ write_tsv(global_new_cases, file.path(data_dir, "summary_global_timeseries_new_c
 
 
 country_specific_new_cases <- new_cases %>%
+  filter(country %in% countries_to_model) %>%
   split(., .$country) %>%
   lapply(function (x) {
     country_name <- x$country[1] %>% tolower() %>% gsub(" ", "", ., fixed = TRUE)
@@ -115,5 +159,19 @@ country_specific_new_cases <- new_cases %>%
     write_tsv(output_df, file.path(data_dir, paste0("summary_", country_name, "_timeseries_new_cases.tsv")))
     output_df
   })
+
+
+province_specific_new_cases <- new_cases %>%
+  filter(province %in% provinces_to_model) %>%
+  split(., .$country) %>%
+  lapply(function (x) {
+    province_name <- x$country[1] %>% tolower() %>% gsub(" ", "", ., fixed = TRUE)
+    output_df <- group_by(x, date) %>%
+      summarize(new_cases=sum(new_cases))
+    write_tsv(output_df, file.path(data_dir, paste0("summary_", province_name, "_timeseries_new_cases.tsv")))
+    output_df
+  })
+
+
 
 save.image("01_curate_time_series.RData")
