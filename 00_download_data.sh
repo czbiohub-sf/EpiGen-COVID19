cd2434553a4462d859408f612fa5ee9b4a041d01
Lucy Li <lucy.li@li-mbp.attlocal.net>
2020-03-31 20:20:15 -0700

    download metadata from Nextstrain

--- 00_download_data.sh
+++ 00_download_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mkdir -p data/time_series/CSSE
-wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -O data/time_series/CSSE/time_series_covid19_confirmed_global.csv
-wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -O data/time_series/CSSE/time_series_covid19_deaths_global.csv
-wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv -O data/time_series/CSSE/time_series_covid19_recovered_global.csv
-wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv -O data/time_series/time_series_covid19_confirmed_US.csv
-wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv -O data/time_series/time_series_covid19_deaths_US.csv
+wget https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -O data/time_series/CSSE/time_series_covid19_confirmed_global.csv
+wget https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -O data/time_series/CSSE/time_series_covid19_deaths_global.csv
+wget https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv -O data/time_series/CSSE/time_series_covid19_recovered_global.csv
+wget https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv -O data/time_series/CSSE/time_series_covid19_confirmed_US.csv
+wget https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv -O data/time_series/CSSE/time_series_covid19_deaths_US.csv
+wget https://github.com/nextstrain/ncov/raw/master/data/metadata.tsv -O data/sequences/gisaid_metadata.tsv
