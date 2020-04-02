f8c9dde05266854c12912d9ffebbb0052c9a6c84
Lucy Li <lucy.li@li-mbp.local>
2020-04-02 15:37:27 -0700

    fixed bug to remove sequences with too many ambiguous sites; removed non-human sequences

--- 02_filter_seq.R
+++ 02_filter_seq.R
@@ -19,7 +19,7 @@ outgroup_id <- "MG772933.1"
 outgroup_fasta <- read.dna(paste0(data_dir, "/", outgroup_id, ".fasta"), "fasta")
 
 # filter sequences with >10% ambiguous bases
-base_freq <- lapply(1:length(fastas), function (i) base.freq(fastas[i]))
+base_freq <- lapply(1:length(fastas), function (i) base.freq(fastas[i], all=TRUE))
 ambiguous_prop <- sapply(base_freq, function (x) sum(x[!(names(x) %in% c("a", "c", "t", "g"))]))
 output_fasta <- fastas[ambiguous_prop<=.1]
 output_fasta$outgroup <- outgroup_fasta
@@ -27,10 +27,12 @@ class(output_fasta) <- "DNAbin"
 epi_id <- fasta_ids[ambiguous_prop<=.1] %>% basename() %>% gsub(".fasta", "", .) %>% c(., outgroup_id)
 names(output_fasta) <- epi_id
 
+seq_metadata <- filter(seq_metadata, host=="Human")
+
 # split sequences by location
 output_fasta_provinces <- filter(seq_metadata, division_exposure %in% provinces_to_model) %>%
   split(., .$division_exposure) %>%
-  lapply(function (x) output_fasta[names(output_fasta) %in% x$gisaid_epi_isl])
+  lapply(function (x) filter(output_fasta[names(output_fasta) %in% x$gisaid_epi_isl]))
 
 output_fasta_countries <- with(seq_metadata, coalesce(country_exposure, country)) %>%
   split(seq_metadata, .) %>%
@@ -56,7 +58,8 @@ mclapply(names(output_fasta_location), function (loc_name) {
     processed_seq <- tail(existing_msa, 1) %>% read.dna("fasta") %>% rownames()
     output_fasta <- output_fasta[!(names(output_fasta) %in% processed_seq)]
   }
-  write.dna(output_fasta, file.path(msa_dir, paste0("input_mafft_", time_str, ".fasta")),
+  write.dna(output_fasta, 
+            file.path(msa_dir, paste0("input_mafft_", time_str, ".fasta")),
             format="fasta", nbcol=1, colw = 80)
 }, mc.cores=detectCores())
 
