d3ea5dfc74de0772ab57f40216660c3c9ca0c635
Lucy Li <lucy.li@li-mbp.local>
2020-04-02 10:14:54 -0700

    use MAFFT

--- 03_multi_sequence_alignment.sh
+++ 03_multi_sequence_alignment.sh
@@ -3,18 +3,16 @@
 
 msadir=$1
 
-inputfile=$(ls $msadir/input_muscle_* | tail -n 1)
+inputfile=$(ls $msadir/input_mafft_* | tail -n 1)
 msafile=${inputfile/input_/msa_}
 if [ ! -f $msafile ]
 then
-  if [[ $(ls $msadir/msa_muscle*) ]]
+  if [[ $(ls $msadir/msa_mafft*) ]]
   then
     # If there is an existing MSA, then add sequences to existing MSA
-    lastest=$(ls $msadir/msa_muscle* | tail -n 1)
-    muscle -in $inputfile -out ${msafile}temp_msa.fasta
-    muscle -profile -in1 $lastest -in2 ${msafile}temp_msa.fasta -out $msafile
-    rm ${msafile}temp_msa.fasta
+    lastest=$(ls $msadir/msa_mafft* | tail -n 1)
+    mafft --add $inputfile $latest > $msafile
   else
-    muscle -in $inputfile -out $msafile
+    mafft $inputfile > $msafile
   fi
 fi
