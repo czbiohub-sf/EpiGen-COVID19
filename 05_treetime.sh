49e991116ac7b9bff145ca4122736fcf1fa5b22d
Lucy Li <lucy.li@li-mbp.attlocal.net>
2020-04-06 14:15:06 -0700

    use TreeTime to estimate branching times

---
+++ 05_treetime.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+treedir=$1
+msadir=${treedir/tree/msa}
+aln=$(ls ${msadir}/msa_mafft_* | tail -n 1)
+treefile=${aln/msa_mafft_/iqtree_}
+treefile=${treefile/.fasta/.treefile}
+treefile=${treefile/msa/tree}
+datefile=${treefile/.treefile/_dates.tsv}
+outdir=$(basename $treefile)
+outdir=${outdir/iqtree_/}
+outdir=${outdir/.treefile/}
+outdir=${treedir}/treetime_$outdir
+
+echo -e "name\tdate" > $datefile
+grep '>' $aln | sed "s/>//" | xargs -I{} grep {} data/sequences/gisaid_metadata.tsv | awk -v OFS='\t' '{print $3,$5}' >> $datefile
+
+mkdir -p $outdir
+treetime --aln $aln --tree $treefile --dates $datefile --outdir $outdir --reroot least-squares --clock-filter 3 --tip-slack 3 --confidence
